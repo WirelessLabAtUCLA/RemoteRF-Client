c7bb73dbd2ec10adf4ec7beb403d5e4d422814ef
Ethan Ge <ethoGalaxy@gmail.com>
2026-08-03 13:19:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="remoterf",
-    version="2.0.6",
+    version="2.0.7",
     author="Ethan Ge",
     author_email="ethoGalaxy@gmail.com",
     description="A python API to remotely access signal centric hardware. Client-side only! Courtesy of Wireless Lab @ UCLA & Prof. Ian Roberts.",
